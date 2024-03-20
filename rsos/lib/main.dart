f5ec25934b06ce63979ea15565b5ef9cc7663a7d
Adrian Szwejkowski <77693969+adrian-szwejk@users.noreply.github.com>
2024-03-19 19:04:45 -0500

    init & setup arch

---
+++ rsos/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:rsos/ui/nav_screens/home/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RSO Search',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'RSO Search Home Page'),
+    );
+  }
+}
