14ce1a83c55d86be7cc44183e627c6bf9a83d32c
Adrian Szwejkowski <77693969+adrian-szwejk@users.noreply.github.com>
2024-04-16 21:31:15 -0500

    fixed amans commit that missed .dart file type

--- rsos/lib/ui/nav_screens/home/home.dart
+++ rsos/lib/ui/nav_screens/home/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:rsos/components/search_filter';
+import 'package:rsos/components/search_filter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
