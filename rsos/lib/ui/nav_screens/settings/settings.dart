08194fa244d873f57636b2daf49e50e3c76c41b8
ashah305 <116755878+ashah305@users.noreply.github.com>
2024-04-30 17:19:56 -0500

    Settings UI (#9)
    lgtm

--- rsos/lib/ui/nav_screens/settings/settings.dart
+++ rsos/lib/ui/nav_screens/settings/settings.dart
@@ -1,4 +1,26 @@
+// import 'package:flutter/material.dart';
+
+// class SettingsPage extends StatefulWidget {
+//   const SettingsPage({super.key});
+
+//   @override
+//   State<SettingsPage> createState() => _SettingsPageState();
+// }
+
+// class _SettingsPageState extends State<SettingsPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: const Text("Settings Page"),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
+import 'package:flutter_settings_ui/flutter_settings_ui.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -8,12 +30,39 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  bool value = false; // Initialize the switch value
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Settings Page"),
+      ),
+      body: SettingsList(
+        sections: [
+          SettingsSection(
+            title: const Text('Section'),
+            tiles: [
+              SettingsTile(
+                title: const Text('Language'),
+                description: const Text( 'English'),
+                leading: const Icon(Icons.language),
+                onPressed: (BuildContext context) {},
+              ),
+              SettingsTile.switchTile(
+                title: const Text('Use fingerprint'),
+                leading: const Icon(Icons.fingerprint),
+                initialValue: value,
+                onToggle: (bool newValue) {
+                  setState(() {
+                    value = newValue;
+                  });
+                },
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
